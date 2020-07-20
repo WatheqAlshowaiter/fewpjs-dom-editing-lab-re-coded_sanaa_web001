c081b96db404fccca8d53d1013efa75ec8aacceb
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-07-20 18:29:02 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -13,9 +13,7 @@ describe("within index.html", () => {
   it("contains a <p> tag", () => {
     expect(document.querySelector('body').innerHTML).to.include('<p>')
   })
-  it("within the <p>, it contains a <strong> tag", () => {
-    expect(document.querySelector('p').innerHTML).to.include('<strong>')
-  })
+  
   it("within the <p>, it contains an <em> tag", () => {
     expect(document.querySelector('p').innerHTML).to.include('<em>')
   })
